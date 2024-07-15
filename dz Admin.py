c5c41e0eb80872bec38f7ae46201f6c36c555841
Anna2791 <annushka279@mail.ru>
2024-07-15 23:25:25 +0300

    done code with a gpt((

--- dz Admin.py
+++ dz Admin.py
@@ -13,25 +13,46 @@
 # 3.Инкапсуляция данных: Убедись, что атрибуты классов защищены от прямого доступа и модификации снаружи.
 # Предоставь доступ к необходимым атрибутам через методы (например, get и set методы).
 class User():
-    def __init__(self,id, name, user):
-        self.id = id
-        self.name = name
-        self.user = user
+    def __init__(self,id, name, access_level = 'посетитель'):
+        self._id = id
+        self._name = name
+        self._access_level = access_level
+
+    def get_id(self):
+        return self._id
+    def get_name(self):
+        return self._name
+    def get_access_level(self):
+        return self._access_level
+
+    def set_name(self, name):
+        self._name = name
+
+    def set_access_level(self, access_level):
+        self._access_level = access_level
+
 class Admin(User):
-    def __init__(self, id, name, user, admin):
-        super().__init__(id, name, user)
-        self.admin = admin
-        self.user = []
-    def add_user(self,id, name, user):
-        self.user.append({'id','name','user'})
-        print(f'Пользователь {id} {name} {user} добавлен в систему')
+    def __init__(self, id, name, access_level = 'админ'):
+        super().__init__(id, name, access_level)
+        self._users = []
+    def add_user(self, user):
+        if isinstance(user, User):
+            self._users.append(user)
+            print(f'Пользователь {user.get_id()} {user.get_name()} добавлен в систему')
+
+    def remove_user(self,user_id):
+        for user in self._users:
+            if user.get_id() == user_id:
+                self._users.remove(user)
+                print(f'Пользователь {user.get_id()} {user.get_name()} удален из системы')
+                return
+        print(f'Пользователь с ID {user_id} не найден в системе')
 
 
-    def remove_user(self):
-        for user in self.user:
-            del self.user[user]
-            print(f'Пользователь{id} {name} {user} удален из системы')
+user1 = User(122, "Борис", 'пользователь')
+user2 = User(1422, "Дрогон", 'пользователь')
+admin1 = Admin(555, "Гога", 'админ')
 
-user1 = User(122, "Борис", 'посетитель')
-admin1 = Admin(555, "Гога", 'сотрудник','админ')
-admin1.add_user(56, 'Марина', 'посетительница')
+admin1.add_user(user2)
+admin1.add_user(user1)
+admin1.remove_user(122)
