55b7d621ddd3021ef1b2aa27ae41722386cc93c7
Anna2791 <annushka279@mail.ru>
2024-07-18 01:29:42 +0300

    create base class

--- dz.py
+++ dz.py
@@ -1,1 +1,15 @@
+# Создайте базовый класс `Animal`, который будет содержать общие атрибуты
+# (например, `name`, `age`) и методы (`make_sound()`, `eat()`) для всех животных.
+class Animal():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def make_sound(self):
+        print('РрррРРРРРррр')
 
+    def eat(self):
+        print('ном-ном-ном')
+
+animal = Animal('лев',2)
+animal.eat()
+animal.make_sound()
